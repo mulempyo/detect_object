02a82d41ff0e5038195b55ab514db4d4541f2c9d
mulempyo <luckyp1@naver.com>
2024-11-30 05:56:32 +0000

    update code

--- src/detect_object.cpp
+++ src/detect_object.cpp
@@ -1,7 +1,6 @@
 #include <dect_object/detect_object.h>
 #include <ros/ros.h>
 #include <costmap_2d/costmap_2d_ros.h>
-#include <std_msgs::Float64.h>
 #include <std_msgs::Bool.h>
 #include <gd_visual_detection_3d_msgs/BoundingBoxes3d.h>
 #include <cmath>
@@ -9,12 +8,6 @@
 #include <geometry_msgs/PointStamped.h>
 
 namespace detect{
-
-void Detect::initialize(ros::NodeHandle& nh){
-    sub = nh.subscribe("/darknet_ros_3d/bounding_boxes", 10, boundingBoxCallback);
-    pub = nh.advertise<std_msgs::Float64>("3d_detection_object_distance",10);
-    pub1 = nh.advertise<std_msgs::Bool>("person_probabilty",10);
-}
 
 void Detect::subCostmap(costmap_2d::Costmap2DROS* costmap_ros){ //using local planner
     costmap_ros_ = costmap_ros;
@@ -25,7 +18,7 @@ void Detect::boundingBoxCallback(const gd_visual_detection_3d_msgs::BoundingBoxe
 
     for (const auto& box : msg->bounding_boxes)
     {
-        if (box.Class == "person" || box.Class == "chair" || box.Class == "pottedplant")
+        if (box.Class == "person" || box.Class == "pottedplant")
         {
             center_x = (box.xmax + box.xmin)/2;
             center_y = (box.ymax + box.ymin)/2;
@@ -33,18 +26,31 @@ void Detect::boundingBoxCallback(const gd_visual_detection_3d_msgs::BoundingBoxe
 
             distance = std::sqrt(center_x * center_x + center_y * center_y + center_z * center_z);
 
+            float obstacle_x = 5.608604;
+            float obstacle_y = 1.296688;
+            float obstacle_z = 0.18;
+
+            float theta = std::atan2(center_y, center_x);
+
             geometry_msgs::PointStamped current_pose_;
             costmap_ros_->getRobotPose(current_pose_);
 
-            cameraToWorld(center_x, center_y, current_pose_.pose.position.x, current_pose_.pose.position.y, 
-                        tf2::getYaw(robot_vel_tf.pose.orientation));
+            cameraToWorld(obstacle_x, obstacle_y, distance, theta, current_pose_.pose.position.x, current_pose_.pose.position.y);
 
             if(box.Class == "person" && box.probability >= 50){
              detected_person = true;
+            }else{
+              detected_person = false;
             }
 
-            distance_msg.data = distance;
-            pub.publish(distance_msg);
+            if(box.Class == "pottedplant"){
+             detected_pottedplant = true;
+            }else{
+              detected_pottedplant = false;
+            }
+
+            detect_pottedplant_msg.data = detected_pottedplant;
+            pub.publish(detect_pottedplant_msg);
 
             detected_person_msg.data = detected_person;
             pub1.publish(detected_person_msg);
@@ -52,9 +58,11 @@ void Detect::boundingBoxCallback(const gd_visual_detection_3d_msgs::BoundingBoxe
     }
   }
 
-  void Detect::cameraToWorld(double center_x, double camera_y, double robot_pose_x, double robot_pose_y, double robot_pose_theta){
-    world_x = robot_pose_x + (center_x * std::cos(robot_pose_theta) - center_y * std::sin(robot_pose_theta));
-    world_y = robot_pose_y + (center_x * std::sin(robot_pose_theta) + center_y * std::cos(robot_pose_theta));
+  void Detect::cameraToWorld(float obstacle_x, float obstacle_y, float distance, float theta, float robot_x, float robot_y){
+    world_x = obstacle_x + distance * std::cos(theta);
+    world_y = obstacle_y + distance * std::sin(theta);
+    world_yaw = std::atan2(world_y - robot_y, world_x - robot_x);
+    camera_yaw = world_yaw - theta;
   }
 
   float Detect::camera_x(){
@@ -66,6 +74,10 @@ void Detect::boundingBoxCallback(const gd_visual_detection_3d_msgs::BoundingBoxe
     camera_y = world_y;
     return camera_y;
   }
+
+  float Detect::camera_yaw(){
+    return camera_yaw;
+  }
 } //namespace
 
 int main(int argc, char** argv)
@@ -73,8 +85,10 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "bounding_boxes_3d_distance");
     ros::NodeHandle nh;
     
-    detect::Detect detector;
-    detector.initialize(nh);
+    sub = nh.subscribe("/darknet_ros_3d/bounding_boxes", 10, &Detect::boundingBoxCallback, this);
+    pub = nh.advertise<std_msgs::Bool>("3d_object_detection_true",10);
+    pub1 = nh.advertise<std_msgs::Bool>("person_probabilty",10);
+
     ros::spin();
     return 0;
 }
